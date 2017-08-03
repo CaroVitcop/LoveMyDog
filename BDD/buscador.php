61e2781744d6bfb333026ddf07d5da97f0cb3247
FLORENCIA VIDAL <42567367@ort>
2017-08-03 12:01:08 -0300

    subo parte 2

--- BDD/buscador.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('config.php');
-$statment=$db->prepare("SELECT * FROM usuarios");
-$statment->execute();
-$results=$statment->fetchAll(PDO::FETCH_ASSOC);
-$json=json_encode($results);
-echo $json;
-	
-?>
--- BDD/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-try{
-	$db=new PDO ('mysql:host=localhost;dbname=lmp;charset=utf8mb4','root','albi');
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
-}catch(PDOexception $e){
-	echo 'ERROR';
-}
